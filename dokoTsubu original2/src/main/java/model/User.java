d8920f0d0f565490f42ed2967b60fd3d520fb687
Fujikawa Ryutaro <l721127@wakayama-u.ac.jp>
2024-10-30 17:34:05 +0900

    ログイン情報のモデルを作成しました

---
+++ dokoTsubu original2/src/main/java/model/User.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	private String userId;
+	private String pass;
+	
+	public User() {}
+	public User(String userId,String pass) {
+		this.userId = userId;
+		this.pass = pass;
+	}
+	
+	public String getUserId() {return userId;}
+	public String getPass() {return pass;}
+}
