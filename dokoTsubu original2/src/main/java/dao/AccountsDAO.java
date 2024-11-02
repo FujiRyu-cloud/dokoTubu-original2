65bd29d60ade4b35771352c1719bbb37ecf7c703
Fujikawa Ryutaro <l721127@wakayama-u.ac.jp>
2024-11-02 16:02:55 +0900

    ログイン処理に関するモデルとサーブレットが完成しました

--- dokoTsubu original2/src/main/java/dao/AccountsDAO.java
+++ dokoTsubu original2/src/main/java/dao/AccountsDAO.java
@@ -1,16 +1,54 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Account;
+import model.User;
+
 public class AccountsDAO {
 	private final String JDBC_URL=
-			"";
+			" jdbc:h2:tcp://localhost/~/dokoOriginal_2";
 	private final String DB_USER = "sa";
 	private final String DB_PASS = "";
 	
-	try {
-		Class.forName("org.h2.Driver");
-	}catch(ClassNotFoundException e){
-		throw new IllegalStateException(
-				"JDBCドライバを読み込めませんでした");
+	public Account findByLogin(User user) {
+		Account account = null;
+		
+		try {
+			Class.forName("org.h2.Driver");
+		}catch(ClassNotFoundException e){
+			throw new IllegalStateException(
+					"JDBCドライバを読み込めませんでした");
+		}
+		
+		try(Connection conn = DriverManager.getConnection(
+				JDBC_URL, DB_USER, DB_PASS)){
+			String sql = "SELECT * FROM ACCOUNTS WHERE USER_ID = ? AND PASS = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1,user.getUserId());
+			pStmt.setString(2, user.getPass());
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			if(rs.next()) {
+				String userId = rs.getString("USER_ID");
+				String pass = rs.getString("PASS");
+				String mail = rs.getString("MAIL");
+				String name = rs.getString("NAME");
+				int age = rs.getInt("AGE");
+				
+				account = new Account(userId,pass,mail,name,age);
+			}	
+				
+		}catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+		return account;
 		
 	}
 }
--- dokoTsubu original2/src/main/java/servlet/LoginServlet.java
+++ dokoTsubu original2/src/main/java/servlet/LoginServlet.java
@@ -2,11 +2,15 @@ package servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.LoginLogic;
+import model.User;
 
 
 @WebServlet("/LoginServlet")
@@ -14,11 +18,30 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(
+				"WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.setCharacterEncoding("UTF-8");
+		String userId = request.getParameter("userId");
+		String pass = request.getParameter("pass");
+		
+		User user = new User(userId,pass);
+		LoginLogic loginLogic = new LoginLogic();
+		boolean islogin = loginLogic.execute(user);
+		
+		if(islogin) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", user);
+			
+			RequestDispatcher dispacher = request.getRequestDispatcher(
+					"WEB-INF/jsp/loginResult.jsp");
+			dispacher.forward(request, response);
+		}else {
+			response.sendRedirect("LoginServlet");
+		}
 	}
 
 }
